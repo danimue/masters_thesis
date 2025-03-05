e2e5ca764f60647add00dca54ab8f570da21cbf9
danimue <daniel.mueller92@gmail.com>
2025-03-05 13:31:17 +0100

    Added a bunch of changes

--- utils.py
+++ utils.py
@@ -14,9 +14,9 @@ def check_dataset():
     # I sometimes have trouble with the working directory not being the same as the script directory.
     # Therefore I include this little bit, changing cwd to script directory.
     # Otherwise I would not be able to use relative paths.
-    
-    print("Current working directory:", os.getcwd())
-    print("Script location:", os.path.abspath(__file__))
+
+    # print("Current working directory:", os.getcwd())
+    # print("Script location:", os.path.abspath(__file__))
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
     if os.getcwd() != script_dir:
@@ -40,14 +40,13 @@ def check_dataset():
         response = requests.get(zip_url)
         if response.status_code != 200:
             raise Exception(f"Failed to download repository: {response.status_code}, {response.text}")
-        
+
         with open(zip_path, "wb") as f:
             f.write(response.content)
             print(f"Repository downloaded as {zip_path}")
 
         with zipfile.ZipFile(zip_path) as archive:
             for file in archive.namelist():
-                print(file)
                 if file.startswith('ARC-AGI-master/data/'):
                     archive.extract(file)
 
@@ -58,19 +57,19 @@ def check_dataset():
         # Move the entire 'data' folder
         if os.path.exists(source_dir):
             shutil.move(source_dir, destination_dir)
-        
+
         # Clean up the zip file and leftover extraction file
         os.remove(zip_path)
         os.rmdir('./ARC-AGI-master/')
         print(f"Cleaned up zip file and leftover extraction file.")
-        
+
         print("Success!")
 
 
 def load_files_from_json() -> tuple:
     """
     Loads JSON files from the 'training' and 'evaluation' ARC data.
-    
+
     Returns:
         tuple: A tuple containing two dictionaries:
             - train (dict): Data from the training directory.
@@ -94,26 +93,24 @@ def load_files_from_json() -> tuple:
     # Load eval
     for file_name in os.listdir(eval_data_dir):
         file_path = os.path.join(eval_data_dir, file_name)
-       
+
         with open(file_path, "r") as f:
             task_name = os.path.splitext(file_name)[0]  # Remove file extension
             eval[task_name] = json.load(f)
-    
+
     return train, eval
 
 
 # converts dict to a nicely formatted string for prompt creation
-def json_to_string(task: dict) -> str:
+def json_to_string(task: dict, include_test: bool = False) -> str:
     """
     Converts a given ARC-task from its json-like form to a string, so that it can be used in a prompt.
-    
-    
+
     Parameters:
-        - task (str): string identifier for each task
+        - task (dict): the task from train or eval set
+        - include_test (bool): by default, the test case will not be included
     """
 
-    #TO-DO: Make it so that you can reference tasks by string identifier or loop over train/eval dict?
-    
     train_tasks = task["train"]
     test_task = task["test"]
 
@@ -135,48 +132,113 @@ def json_to_string(task: dict) -> str:
                 final_output += f"\n{str(row)},"
         final_output += "]\n\n"
 
-    # Test Case
-    # omit test case for now
-    # final_output += "Test\n["
-    # for row in test_task[0]["input"]:
-    #     final_output += f"\n{str(row)}"
-    # final_output += "]"
+    # Test Example
+    if include_test:
+        final_output += "Test\n["
+        for row in test_task[0]["input"]:
+            final_output += f"\n{str(row)}"
+        final_output += "]"
 
     return final_output
 
 
-# def find_task(task_identifier: str, *dicts: Dict[str, Any]) -> Any:
-#     """_summary_
-
-#     Args:
-#         task_identifier (str): _description_
-#         dicts (Dict[str, Any]): _description_
-#     Returns:
-#         Any: _description_
-#     """
-#     for d in dicts:
-#         if task_identifier in d:
-#             return d[task_identifier]
-#     return None
-
-
-def create_prompt(task_identifier: str, *dicts: Dict[str, Any]) -> str:
+def json_to_ascii(task: dict[str, Any], include_test: bool = False) -> str:
     
+    result_str = "Training examples as character representation:\n"
+    
+    # TODO: come up with better letters? maybe b for black etc.
+    letter_mapping = {0: ' ', 1: 'b', 2: 'c', 3: 'd', 4: 'e',
+                      5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j'}
+    
+    # Training examples
+    for i, element in enumerate(task["train"]):        
+        result_str += f"Example {i + 1}: Input\n"
+        
+        # Input
+        for line in element["input"]:
+            for index, int in enumerate(line):
+                if index == len(line) -1:
+                    result_str += letter_mapping[int]
+                else:
+                    result_str += letter_mapping[int] + "|"
+            result_str += "\n"
+        result_str += "\n\n"
+        
+        # Output
+        result_str += f"Example {i + 1}: Output\n"
+        for line in element["output"]:
+            for index, int in enumerate(line):
+                if index == len(line) -1:
+                    result_str += letter_mapping[int]
+                else:
+                    result_str += letter_mapping[int] + "|"
+            result_str += "\n"
+        result_str += "\n\n"
+        
+    # Test case
+    if include_test:
+        result_str += "Test case:\n"
+        
+        for line in task["test"][0]["input"]:
+            for index, int in enumerate(line):
+                if index == len(line) -1:
+                    result_str += letter_mapping[int]
+                else:
+                    result_str += letter_mapping[int] + "|"
+            result_str += "\n"
+
+    return result_str
+
+
+def find_task(task_identifier: str, *dicts: dict[str, Any]) -> dict[str, Any] | None:
+    """
+    Searches for task because it could be in either train or eval dict.
+
+    Parameters:
+        task_identifier (str): easy way to access the task via their string
+        *dicts (dict[str, Any]): should always be train and eval dictionary
+
+    Returns:
+        part of the dict that the task is in or
+        None if task was not found
+    """
     for d in dicts:
         if task_identifier in d:
-            task = d[task_identifier]
-    
-    if task != None:
-        stringified_grid = json_to_string(task)
-    
-    task_explanation = """Here are are some examples of the transformation from `input` to `output`: \n
-"""
+            return d[task_identifier]
+    return None
 
-    # task_instructions = """\n\nPlease return the missing output grid with the same formatting as the examples. Do not write code, do not explain your solution - just answer with the correct grid."""
+
+def create_prompt(task: dict[str, Any], prompt_type: str = 'simple', reason: bool = True) -> str | None:
+    #prompt = ""
     
-    task_examples = stringified_grid
-    
-    task_instructions = """\n\nYou'll need to carefully reason in order to determine the transformation rule. Start your response by carefully reasoning in <reasoning></reasoning> tags. Then, implement the transformation in code.
+    if prompt_type == 'simple':
+        task_explanation = """The examples below show input-output pairs of numerical representations of visual grids. Each integer value represents a colored square on this grid. There are 10 possible colors (and therefore integer values) - 0 to 9, each representing a color. Each example input grid is transformed into an output grid. Each integer value represents a colored square on that grid. 
+The input is transformed into the corresponding output using the same transformation rule every time. Your task is to analyse this transformation rule by observing the examples and then solve the test-case.\n\n"""
+
+        task_examples = json_to_string(task, include_test=True)
+        
+        grid_representation_char = json_to_ascii(task, include_test=True)
+        task_examples_char = f"""\n\nHere is another representation of the grids given to you. These examples are the same as in the list[list[int]] representation.
+They are given to you as an additional help to understand the problem. This representation shows "black" / integer 0 parts as empty. This is because black is usually
+the background color of the grid and it is supposed to make it easier to detect objects in the grid.
+
+{grid_representation_char}"""
+        
+        if reason:        
+            task_instructions = """\n\nNow it is your turn. Please reason about what the transformation rule could be. Put your thoughts between <reasoning> ... </reasoning> tags. Then, solve the test case and provide the correct output.
+Please always format your answer like this, as correct formatting is crucial to this task:
+Answer: ``` <your answer> ```"""
+        else:
+            task_instructions = """\n\nNow it is your turn to solve the test case given to you. Do NOT reason about the thought process.
+Do NOT output an explanation of how you get to the answer. ONLY output the correct grid for the test case. Format your answer like this:
+Answer: ``` <your answer> ``` Make sure to format your answer as a list of lists of integers, just like the examples shown to you."""
+
+        return task_explanation + task_examples + task_instructions
+
+    elif prompt_type == 'program_synthesis':
+        task_explanation = """Here are are some examples of the transformation from `input` to `output`: \n"""
+        task_examples = json_to_string(task, include_test=False)
+        task_instructions = """\n\nYou'll need to carefully reason in order to determine the transformation rule. Start your response by carefully reasoning in <reasoning></reasoning> tags. Then, implement the transformation in code.
 
 For your reasoning, please pay close attention to where the objects in the grid are in the input and where they are moved in the output example. You can consider a space with a 0 as an empty space, as 0 represents the color black.
 
@@ -187,4 +249,26 @@ After your reasoning is completed write code in triple backticks (```python and 
 
 Don't write tests in your python code, just output the `transform` function (it will be tested later)."""
 
-    return task_explanation + task_examples + task_instructions
+        return task_explanation + task_examples + task_instructions
+
+    else:
+        print("No valid prompt type specified...")
+        return None
+    
+    
+def load_config(filepath="config.json"):
+    """Loads the configuration from a JSON file."""
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+
+def get_template_config(config, template_name=None):
+    """Retrieves a specific template's configuration."""
+    if template_name is None:
+        template_name = config.get("default_template")
+
+    templates = config.get("templates")
+    if templates and template_name in templates:
+        return templates[template_name]
+    else:
+        raise ValueError(f"Template '{template_name}' not found in config.")
